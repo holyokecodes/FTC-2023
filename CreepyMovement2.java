ea30769e81f43d7db40b0aa915922c8259f19366
Sawyer Perreault <121728792+GeicoLizard111@users.noreply.github.com>
2023-01-05 12:29:44 -0500

    cleanup

--- CreepyMovement2.java
+++ CreepyMovement2.java
@@ -1,12 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+//imports
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Blinker;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gyroscope;
-
 import com.arcrobotics.ftclib.geometry.Translation2d;
 import com.arcrobotics.ftclib.geometry.Pose2d;
 import com.arcrobotics.ftclib.kinematics.wpilibkinematics.MecanumDriveKinematics;
@@ -16,7 +16,6 @@ import com.arcrobotics.ftclib.hardware.RevIMU;
 import com.arcrobotics.ftclib.geometry.Rotation2d;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.drivebase.MecanumDrive;
-
 import com.arcrobotics.ftclib.purepursuit.waypoints.StartWaypoint;
 import com.arcrobotics.ftclib.purepursuit.waypoints.GeneralWaypoint;
 import com.arcrobotics.ftclib.purepursuit.waypoints.EndWaypoint;
@@ -28,60 +27,66 @@ import com.arcrobotics.ftclib.purepursuit.Waypoint;
 public class CreepyMovement2 extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-    
+
+        //setup time
         ElapsedTime time = new ElapsedTime();
+
+        //setup revIMU
         RevIMU imu = new RevIMU(hardwareMap);
         imu.init();
-        
+
+
+        //setup motors
         Motor frontLeftMotor = new Motor (hardwareMap, "frontLeft");
         Motor frontRightMotor = new Motor (hardwareMap, "frontRight");
         Motor backLeftMotor = new Motor (hardwareMap, "backLeft");
         Motor backRightMotor = new Motor (hardwareMap, "backRight");
-        
-    
+
         MecanumDrive driveBase = new MecanumDrive (frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor);
 
-        
+        //declare variables
         double strafeSpeed;
         double fowardSpeed;
         double rotateSpeed;
         double heading;
-        
+
+        //wait for the game to start
         waitForStart();
-        
-       
+
+        //run until the game is over
         while(opModeIsActive() && !isStopRequested()) {
-           
-            
-           
-           
-        
+
+
+            //stop the robot
             requestOpModeStop();
-        
-            
-        }    
+
+        }
     }
-    
+
+
+    //setup methods
+
+
     public void driveFoward() {
-      
+
         double startTime = getRuntime();
 
         double strafeSpeed = 0;
         double fowardSpeed = 1;
         double rotateSpeed = 0;
         double heading = 0;
-            
-      
+
+
     }
-    
-        public void drive(double strafeSpeed, double startTime, double fowardSpeed, double rotateSpeed, double heading ) {
-    
+
+    public void drive(double strafeSpeed, double startTime, double fowardSpeed, double rotateSpeed, double heading ) {
+
         while(getRuntime() - startTime < time && opModeIsActive()) {
-    
-           this.driveBase.driveFieldCentric(strafeSpeed, fowardSpeed, rotateSpeed,heading);
-        
-           }  
-    
-    
+
+            this.driveBase.driveFieldCentric(strafeSpeed, fowardSpeed, rotateSpeed,heading);
+
+        }
+
+
     }
 }
